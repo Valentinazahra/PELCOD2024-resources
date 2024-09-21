a945b36b1bb135f9a1762dcfe1a962877e22f663
valentinazahra <valentinazahrafebrianti@gmail.com>
2024-09-21 15:15:06 +0700

    latihan coding 21 september 2024

---
+++ main.py
@@ -0,0 +1,41 @@
+# nilai = int(input("masukkan nilai: "))
+# print(nilai)
+
+# nilai = nilai + 5
+# print(nilai)
+
+# if (nilai > 80):
+#     print("selamat anda lulus")
+# elif (nilai > 90 and nilai <= 100):
+#     print("selamat anda lulus, dengan nilai yang memuaskan")    
+# else:
+#     print("anda masih belum lulus tes, semangat dan coba lagi")    
+
+# fof loop
+# for i in range(1, 10, 2):
+#     print(i)
+#     print("hello world")
+
+#while loop
+
+# angka = 10
+# while(angka > 1):
+#     print(angka)
+#     angka -= 1 #angka = angka - 1
+
+# FUNCTION
+#def
+# 
+#     print("hello zahra")
+#     print("selamat pagi")
+# sayHello()
+# sayHello()
+# sayHello()
+# sayHello()
+
+#lambda
+fungsi_kali2 = lambda param_angka : param_angka * 2
+angka = 10
+print(fungsi_kali2(angka))
+
+
